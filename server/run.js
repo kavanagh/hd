d6aa217ae785abed4dcd5806bc883f6006036ecb
Callum Locke <callum.locke@gmail.com>
2013-05-20 18:59:49 +0100

    use redis

--- server/run.js
+++ server/run.js
@@ -25,42 +25,59 @@
 */
 
 var express = require('express');
-
 var app = express();
-var staticPath = require('path').resolve(__dirname + '/../dist');
 
-// Load dummy data
-require('coffee-script');
-var dummyData = require('./dummy');
-
+// Use middleware
 app.use(express.logger());
+var staticPath = require('path').resolve(__dirname + '/../dist');
 app.use(express.static(staticPath));
 
-// Endpoints
+// Connect to Redis
+var redisUrl = process.env.REDISTOGO_URL || 'redis://localhost:6379';
+var redis = require('redis-url').connect(redisUrl);
 
+// Routes
 app.get('/question', function (req, res) {
+  var response;
+  var sendResponse = function (status) {
+    if (status != null)
+      res.status(status);
+    res.jsonp(response);
+  };
+
   // Look up question
+  redis.hgetall('article:'+req.query.article, function (err, obj) {
+    if (err) throw err;
 
-  var response = dummyData.questions[req.query.article];
+    if (obj == null) {
+      response = {error: 'Article ID not found: ' + req.query.article};
+      sendResponse(404);
+    }
+    else {
+      response = obj;
+      response.articleId = req.query.article;
 
-  if (typeof response !== 'object') {
-    response = {
-      error: 'No question found for article'
-    };
-    res.status(404);
-  }
-  else {
-    // Add the article ID
-    response.articleId = req.query.article;
-    
-    // Add this user's response
-    // TODO
-  }
+      if (req.query.user) {
+        // Echo back the user ID in the response
+        response.userId = req.query.user;
 
-  res.jsonp(response);
+        // Look up this user's vote (key format: "vote:USERID:ARTICLEID")
+        redis.hgetall('vote:' + req.query.user + ':' + req.query.article, function (err, obj) {
+          if (err) throw err;
+
+          response.vote = obj;
+          
+          sendResponse();
+        });
+      }
+      else {
+        sendResponse();
+      }
+    }
+  });
 });
 
-
+// Start the app
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
