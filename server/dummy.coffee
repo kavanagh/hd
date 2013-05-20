146a7a757cc517b3af93b5f533c0975a6107c700
Callum Locke <callum.locke@gmail.com>
2013-05-20 17:56:54 +0100

    wip - question endpoint

---
+++ server/dummy.coffee
@@ -0,0 +1,10 @@
+module.exports =
+  questions:
+    'someid':
+      question: 'What is your reaction to this interesting question?'
+      xTicks: 1
+      yTicks: 4
+    'anotherid':
+      question: 'What do you think of this?'
+      xTicks: 4
+      yTicks: 4
--- server/run.js
+++ server/run.js
@@ -5,12 +5,13 @@
 
     GET /question?article=ARTICLE_ID&user=USER_ID
       Returns:
-        userId
-        result - for this user (if any)
-        articleId
         question
         xTicks
         yTicks
+
+        userId
+        result - for this user (if any)
+        articleId
 
     GET /results?article=ARTICLE_ID
       Returns:
@@ -24,15 +25,41 @@
 */
 
 var express = require('express');
+
 var app = express();
 var staticPath = require('path').resolve(__dirname + '/../dist');
+
+// Load dummy data
+require('coffee-script');
+var dummyData = require('./dummy');
 
 app.use(express.logger());
 app.use(express.static(staticPath));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+// Endpoints
+
+app.get('/question', function (req, res) {
+  // Look up question
+
+  var response = dummyData.questions[req.query.article];
+
+  if (typeof response !== 'object') {
+    response = {
+      error: 'No question found for article'
+    };
+    res.status(404);
+  }
+  else {
+    // Add the article ID
+    response.articleId = req.query.article;
+    
+    // Add this user's response
+    // TODO
+  }
+
+  res.jsonp(response);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
