f61ad539aac009d40dd9e5a3c7db5c9b2e17ae40
Callum Locke <callum.locke@gmail.com>
2013-05-21 14:53:06 +0100

    in chrome ext, wait a bit before injecting script into ft.com

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,9 +1,16 @@
-var script = document.createElement('script');
-script.src = '//reaction-c.herokuapp.com/scripts/main.js';
+window.addEventListener('load', function () {
+  
+  setTimeout(function() {
+    var script = document.createElement('script');
+    script.src = '//reaction-c.herokuapp.com/scripts/main.js';
 
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = '//reaction-c.herokuapp.com/styles/main.css';
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '//reaction-c.herokuapp.com/styles/main.css';
 
-document.body.appendChild(script);
-document.body.appendChild(link);
+    document.body.appendChild(script);
+    document.body.appendChild(link);
+
+  }, 1000);
+
+}, false);
